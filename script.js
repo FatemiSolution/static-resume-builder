cde36515ea091599ffa4ada39a3d705be3b92556
FatemiSolution <shabbir.high@gmail.com>
2024-09-07 16:30:38 +0500

    created static resume website

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const toggleSkillsButton = document.getElementById('toggleSkillsButton');
-const skillsSection = document.getElementById('skillsSection');
-
-toggleSkillsButton.addEventListener('click', () => {
-    if (skillsSection.style.display === 'none') {
+// Toggle Skills Section
+var toggleSkillsButton = document.getElementById('toggleSkillsButton');
+var skillsSection = document.getElementById('skillsSection');
+toggleSkillsButton === null || toggleSkillsButton === void 0 ? void 0 : toggleSkillsButton.addEventListener('click', function () {
+    if ((skillsSection === null || skillsSection === void 0 ? void 0 : skillsSection.style.display) === 'none') {
         skillsSection.style.display = 'block';
-    } else {
+    }
+    else {
         skillsSection.style.display = 'none';
     }
 });
-
-const downloadResumeButton = document.getElementById('downloadResumeButton');
-
-downloadResumeButton.addEventListener('click', () => {
-    const element = document.createElement('a');
-    const file = new Blob([document.body.innerHTML], {type: 'text/html'});
+// Download Resume Functionality
+var downloadResumeButton = document.getElementById('downloadResumeButton');
+downloadResumeButton === null || downloadResumeButton === void 0 ? void 0 : downloadResumeButton.addEventListener('click', function () {
+    var element = document.createElement('a');
+    var file = new Blob([document.body.innerHTML], { type: 'text/html' });
     element.href = URL.createObjectURL(file);
     element.download = 'resume.html';
-    document.body.appendChild(element);
+    document.body.appendChild(element); // Required for Firefox
     element.click();
 });
